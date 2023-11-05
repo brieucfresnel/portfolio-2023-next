ae4b16f06ae14bee8e7999a53599d5bed24e2d05
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-05 15:14:14 +0100

    feat: add ProjectsList to main page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Header from "@/components/Header/Header";
+import ProjectsList from "@/components/ProjectsList/ProjectsList";
 import ThreeScene from "@/components/ThreeScene/ThreeScene";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header></Header>
       <ThreeScene></ThreeScene>
+      <ProjectsList></ProjectsList>
     </>
   );
 }
