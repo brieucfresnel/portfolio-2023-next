984be32e2ab6fffc08241dd3d6450513dd80e9dd
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-06 18:59:46 +0100

    feat: add LenisScroller component

---
+++ app/loading.jsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  // return <div style={{ background: 'white', height: '100vh', width: '100vw' }}>Loading...</div>
+}
