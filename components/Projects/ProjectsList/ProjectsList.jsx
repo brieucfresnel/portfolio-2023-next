3e75769cc972d77e26230239c863b30adef4023d
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-08 22:41:23 +0100

    feat: projects list rows hover double effect (wip)

--- components/Projects/ProjectsList/ProjectsList.jsx
+++ components/Projects/ProjectsList/ProjectsList.jsx
@@ -1,8 +1,14 @@
 "use client"
 
-import React, { useState, useEffect, useRef, useLayoutEffect } from "react"
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useLayoutEffect,
+  useCallback,
+} from "react"
 import Image from "next/image"
-import { gsap } from "common/utils/gsap"
+import { gsap, ScrollTrigger } from "common/utils/gsap"
 
 import Container from "@/components/Container/Container"
 import ProjectsListRow from "@/components/Projects/ProjectsListsRow/ProjectsListRow"
@@ -13,12 +19,16 @@ import "./ProjectsList.scss"
 function ProjectsList({ projects }) {
   const wrapperRef = useRef(null)
   const imageRef = useRef(null)
+  const itemBgRef = useRef(null)
+  const listItemsRefs = useRef([])
   const mousePosition = useMousePosition()
   const [currentImageData, setCurrentImageData] = useState(projects[0]?.image)
+  // const [currentHoveredProject, setCurrentHoveredProject] = useState(null)
 
   useEffect(() => {
     const moveImage = () => {
       if (!(mousePosition.x && mousePosition.y)) return
+
       gsap.to(imageRef.current, {
         top: mousePosition.y,
         left: `calc(${mousePosition.x}px + 3vw)`,
@@ -29,25 +39,86 @@ function ProjectsList({ projects }) {
 
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      gsap.set(imageRef.current, { y: "-50%" })
+      const q = gsap.utils.selector(wrapperRef.current)
+      const rowsContainer = q(".projects-list__rows")
 
-      gsap.fromTo(
-        imageRef.current,
-        {
+      let lastHoveredProject = null
+      let currentHoveredProject = null
+
+      const hideMouseHovers = () => {
+        currentHoveredProject = null
+
+        gsap.to([imageRef.current, itemBgRef.current], {
           opacity: 0,
-        },
-        {
+        })
+      }
+
+      const showMouseHovers = () => {
+        console.log("show mouse hovers")
+        gsap.to([imageRef.current, itemBgRef.current], {
           opacity: 1,
-          ease: "power3.inOut",
-          scrollTrigger: {
-            trigger: wrapperRef.current,
-            start: "top 30%",
-            toggleActions: "play reverse play reverse",
-            // markers: true
-          },
-        }
-      )
-    }, [imageRef, wrapperRef])
+        })
+      }
+
+      wrapperRef.current.addEventListener("mouseenter", showMouseHovers)
+      wrapperRef.current.addEventListener("mouseleave", hideMouseHovers)
+
+      const setupImage = () => {
+        gsap.set(imageRef.current, { y: "-50%" })
+      }
+      setupImage()
+
+      const setupHoveredImageBg = () => {
+        listItemsRefs.current.forEach((item) => {
+          const onMouseHover = (e) => {
+            lastHoveredProject = currentHoveredProject
+            currentHoveredProject = item
+            const c = gsap.utils.selector(e.target)
+
+            gsap
+              .timeline()
+              .to(c(".projects-list-row__bg"), {
+                height: "100%",
+                duration: 0.3,
+                ease: "power2.out",
+              })
+              .to(
+                c(".projects-list-row__cell"),
+                {
+                  color: "#070707",
+                  duration: 0.5,
+                },
+                "- 3"
+              )
+              .to(c(".projects-list-row__tech > span"), {
+                borderColor: "#070707",
+              })
+          }
+          const onMouseOut = (e) => {
+            const c = gsap.utils.selector(e.target)
+
+            gsap
+              .timeline()
+              .to(c(".projects-list-row__bg"), {
+                height: "0",
+                duration: 0.3,
+                ease: "power2.out",
+              })
+              .to(c(".projects-list-row__cell"), {
+                color: "#ffe8ec",
+                duration: 0.5,
+              })
+              .to(c(".projects-list-row__tech > span"), {
+                borderColor: "#ffe8ec",
+              })
+          }
+
+          item.addEventListener("mouseenter", onMouseHover)
+          item.addEventListener("mouseleave", onMouseOut)
+        })
+      }
+      setupHoveredImageBg()
+    }, [imageRef, wrapperRef, listItemsRefs])
     return () => ctx.revert() // cleanup
   }, [])
 
@@ -65,19 +136,20 @@ function ProjectsList({ projects }) {
           ></Image>
         </div>
         <h2 className="projects-list__title">Références</h2>
-
+        <div className="projects-list__row projects-list__row--header">
+          <span>Client</span>
+          <span>Type</span>
+          <span></span>
+          <span>Tech</span>
+          <span>By</span>
+          <span>Year</span>
+        </div>
         <div className="projects-list__rows">
-          <div className="projects-list__row projects-list__row--header">
-            <span>Client</span>
-            <span>Type</span>
-            <span></span>
-            <span>Tech</span>
-            <span>By</span>
-            <span>Year</span>
-          </div>
+          <div className="projects-list__item-hover-bg" ref={itemBgRef}></div>
 
-          {projects.sort().map((project, i) => (
+          {projects.map((project, i) => (
             <ProjectsListRow
+              ref={(el) => (listItemsRefs.current[i] = el)}
               className="projects-list__row"
               key={i}
               {...project}
--- components/Projects/ProjectsListsRow/ProjectsListRow.jsx
+++ components/Projects/ProjectsListsRow/ProjectsListRow.jsx
@@ -1,52 +1,43 @@
-import React, { useRef, useLayoutEffect } from "react"
+import React, { useRef, forwardRef, useEffect, useLayoutEffect } from "react"
 import { gsap } from "common/utils/gsap"
 import cn from "classnames"
 
 import "./ProjectsListRow.scss"
 
-function ProjectsListRow({
-  id,
-  title,
-  team,
-  type,
-  tech,
-  image,
-  year,
-  setCurrentImageData,
-  className,
-}) {
-  const el = useRef(null)
+export default forwardRef(function ProjectsListRow(
+  { id, title, team, type, tech, image, year, setCurrentImageData, className },
+  ref
+) {
   const tl = useRef(null)
-
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      const q = gsap.utils.selector(el.current)
-      // const text = new SplitType(q('.projects-list-row__title'));
-      const title = q("a")
-    }, [el, tl])
-
-    return () => ctx.revert() // cleanup
-  }, [])
 
   return (
     <a
       href="/"
-      ref={el}
+      ref={ref}
       className={cn(className, "projects-list-row")}
       onMouseOver={() => setCurrentImageData(image)}
     >
-      <h3 className="projects-list-row__title">{title}</h3>
-      <span className="projects-list-row__type">{type}</span>
-      <span></span>
-      <span className="projects-list-row__tech">
-        {tech.map((tech, i) => (
-          <span key={i}>{tech}</span>
-        ))}
-      </span>
-      <span className="projects-list-row__team">{team}</span>
-      <span className="projects-list-row__year">{year}</span>
+      <div className="projects-list-row__bg"></div>
+      <div className="projects-list-row__content">
+        <h3 className="projects-list-row__cell projects-list-row__title">
+          {title}
+        </h3>
+        <span className="projects-list-row__cell projects-list-row__type">
+          {type}
+        </span>
+        <span className="projects-list-row__cell"></span>
+        <span className="projects-list-row__cell projects-list-row__tech">
+          {tech.map((tech, i) => (
+            <span key={i}>{tech}</span>
+          ))}
+        </span>
+        <span className="projects-list-row__cell projects-list-row__team">
+          {team}
+        </span>
+        <span className="projects-list-row__cell projects-list-row__year">
+          {year}
+        </span>
+      </div>
     </a>
   )
-}
-
-export default ProjectsListRow
+})
