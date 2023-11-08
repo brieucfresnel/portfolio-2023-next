aff6e7dcdff66c6bc497f0c553a9882f15345e77
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-08 13:22:21 +0100

    fix: font relative paths

--- app/fonts.js
+++ app/fonts.js
@@ -1,43 +1,43 @@
-import localFont from "next/font/local";
+import localFont from "next/font/local"
 
 export const ppNeueMachina = localFont({
   variable: "--pp-neue-machina",
   src: [
     {
-      path: "../assets/fonts/PPNeueMachina-PlainRegular.otf",
+      path: "assets/fonts/PPNeueMachina-PlainRegular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../assets/fonts/PPNeueMachina-PlainRegularItalic.otf",
+      path: "assets/fonts/PPNeueMachina-PlainRegularItalic.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../assets/fonts/PPNeueMachina-PlainUltraBold.otf",
+      path: "assets/fonts/PPNeueMachina-PlainUltraBold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../assets/fonts/PPNeueMachina-PlainUltraBoldItalic.otf",
+      path: "assets/fonts/PPNeueMachina-PlainUltraBoldItalic.otf",
       weight: "700",
       style: "italic",
     },
   ],
-});
+})
 
 export const ppNeueMachinaInktrap = localFont({
   variable: "--pp-neue-machina-inktrap",
   src: [
     {
-      path: "../assets/fonts/PPNeueMachina-InktrapRegular.otf",
+      path: "assets/fonts/PPNeueMachina-InktrapRegular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../assets/fonts/PPNeueMachina-InktrapUltraBold.otf",
+      path: "assets/fonts/PPNeueMachina-InktrapUltraBold.otf",
       weight: "700",
       style: "normal",
     },
   ],
-});
+})
