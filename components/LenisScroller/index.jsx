7b97c8309da9a1275c0eaf9addb967bade052952
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-08 12:00:30 +0100

    fix: integrate Lenis to gsap ticker

--- components/LenisScroller/index.jsx
+++ components/LenisScroller/index.jsx
@@ -1,34 +1,35 @@
-"use client";
-import Lenis from "@studio-freight/lenis";
-import { useEffect, useRef } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+"use client"
+import Lenis from "@studio-freight/lenis"
+import { useEffect, useRef } from "react"
+import { usePathname, useSearchParams } from "next/navigation"
+import { gsap, ScrollTrigger } from "common/utils/gsap"
 
 export const LenisScroller = () => {
-  const lenis = useRef(null);
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const lenis = useRef(null)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
   useEffect(() => {
-    if (lenis.current) lenis.current.scrollTo(0, { immediate: true });
-  }, [pathname, searchParams, lenis]);
+    if (lenis.current) lenis.current.scrollTo(0, { immediate: true })
+  }, [pathname, searchParams, lenis])
 
   useEffect(() => {
-    lenis.current = new Lenis();
+    lenis.current = new Lenis({})
 
-    lenis.current.on("scroll", (e) => {
-    });
+    lenis.current.on("scroll", ScrollTrigger.update)
 
-    function raf(time) {
-      lenis.current.raf(time);
-      requestAnimationFrame(raf);
-    }
+    lenis.current.on("scroll", (e) => {})
 
-    requestAnimationFrame(raf);
+    gsap.ticker.add((time) => {
+      lenis.current.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
 
     return () => {
-      lenis.current.destroy();
-      lenis.current = null;
-    };
-  }, []);
-  return <></>;
-};
+      lenis.current.destroy()
+      lenis.current = null
+    }
+  }, [])
+  return <></>
+}
