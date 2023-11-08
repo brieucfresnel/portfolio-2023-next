7d6ed5ae4733989cd2dbe28078624005e44fdb92
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-08 13:15:20 +0100

    chore: rm ProjectsSlider from page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
-import Header from "@/components/Header/Header";
-import Projects from "@/components/Projects/Projects";
-import ThreeScene from "@/components/ThreeScene/ThreeScene";
+import Header from "@/components/Header/Header"
+import Footer from "@/components/Footer/Footer"
+import Projects from "@/components/Projects/Projects"
+import ThreeScene from "@/components/ThreeScene/ThreeScene"
 
 export default function Home() {
   return (
     <>
       <Header></Header>
-      {/* <ThreeScene></ThreeScene> */}
+      <ThreeScene></ThreeScene>
       <Projects></Projects>
+      <Footer></Footer>
     </>
-  );
+  )
 }
