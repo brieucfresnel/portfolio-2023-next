8818526e86024a6ad82a5d3f54c1a53928de8bdd
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-09 11:33:19 +0100

    feat: add footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,17 +5,23 @@ import "./Footer.scss"
 export default function Footer() {
   return (
     <footer className="footer">
-      <Container>
-        <div className="footer__main">
+      <div className="footer__main">
+        <Container>
           <div className="footer__text">Vous cherchez un développeur ?</div>
           <a className="footer__link" href="mailto:fresnel.brieuc@gmail.com">
             fresnel.brieuc@gmail.com
           </a>
-        </div>
-        <div className="footer__bot">
-          <div className="footer__copyright"></div>
-        </div>
-      </Container>
+        </Container>
+      </div>
+
+      <div className="footer__bot">
+        <Container>
+          <div className="footer__copyright">
+            © Brieuc Fresnel {new Date().getFullYear()}
+          </div>
+          <div className="footer__back-to-top back-to-top"></div>
+        </Container>
+      </div>
     </footer>
   )
 }
