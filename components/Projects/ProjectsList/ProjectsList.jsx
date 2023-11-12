46bbc6a7f1b5584ae95b279fbe6c13bcbbe9c93c
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-12 13:12:49 +0100

    fix: move projects-list-row hover effect to each item instead of parent

--- components/Projects/ProjectsList/ProjectsList.jsx
+++ components/Projects/ProjectsList/ProjectsList.jsx
@@ -1,15 +1,8 @@
 "use client"
 
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useLayoutEffect,
-  useCallback,
-} from "react"
-import Image from "next/image"
+import React, { useRef, useLayoutEffect } from "react"
 import { gsap, ScrollTrigger } from "common/utils/gsap"
-import { chivo } from "@/common/utils/fonts"
+import { useMediaQuery } from "@/common/utils/media-queries"
 
 import Container from "@/components/Container/Container"
 import ProjectsListRow from "@/components/Projects/ProjectsListsRow/ProjectsListRow"
@@ -20,141 +13,17 @@ import "./ProjectsList.scss"
 function ProjectsList({ projects }) {
   const wrapperRef = useRef(null)
   const itemBgRef = useRef(null)
-  const listItemsRefs = useRef([])
   const mousePosition = useMousePosition()
-
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      let currentHoveredProject = null
-
-      const hideMouseHovers = () => {
-        currentHoveredProject = null
-
-        gsap.to(itemBgRef.current, {
-          opacity: 0,
-        })
-      }
-      wrapperRef.current.addEventListener("mouseleave", hideMouseHovers)
-
-      const showMouseHovers = () => {
-        gsap.to(itemBgRef.current, {
-          opacity: 1,
-        })
-      }
-      wrapperRef.current.addEventListener("mouseenter", showMouseHovers)
-
-      const setupHoveredImageBg = () => {
-        listItemsRefs.current.forEach((item) => {
-          const c = gsap.utils.selector(item)
-          let animation = null
-          let isHovering = false
-
-          const clrBorderLight = "#393435"
-          const clrDark = "#070707"
-          const clrLight = "#ffe8ec"
-          const clrBgLight = ""
-
-          const onMouseEnter = (e) => {
-            isHovering = true
-
-            if (!animation) {
-              animation = gsap
-                .timeline({
-                  onComplete: () => {
-                    animation = null
-                    if (!isHovering) {
-                      onMouseLeave()
-                    }
-                  },
-                })
-                .add("background")
-                .to(c(".projects-list-row__bg"), {
-                  height: "100%",
-                  duration: 0.3,
-                  ease: "power2.out",
-                })
-                .to(item, {
-                  borderColor: clrLight,
-                })
-                .to(
-                  c(".projects-list-row__cell"),
-                  {
-                    color: clrDark,
-                    duration: 0.3,
-                  },
-                  "background"
-                )
-                .to(
-                  c(".projects-list-row__tech > span"),
-                  {
-                    borderColor: clrDark,
-                    color: clrDark,
-                  },
-                  "background"
-                )
-            }
-          }
-
-          const onMouseLeave = (e) => {
-            isHovering = false
-
-            if (!animation) {
-              animation = gsap
-                .timeline({
-                  onComplete: () => {
-                    animation = null
-                    if (isHovering) {
-                      onMouseEnter()
-                    }
-                  },
-                })
-                .add("background")
-                .to(c(".projects-list-row__bg"), {
-                  height: "0",
-                  duration: 0.3,
-                  ease: "power2.out",
-                })
-                .to(item, {
-                  borderColor: clrBorderLight,
-                })
-                .to(
-                  c(".projects-list-row__cell"),
-                  {
-                    color: clrLight,
-                    duration: 0.3,
-                  },
-                  "background"
-                )
-                .to(
-                  c(".projects-list-row__tech > span"),
-                  {
-                    color: clrLight,
-                    borderColor: clrLight,
-                  },
-                  "background"
-                )
-                .to(c(".projects-list-row__title"), {}, "background")
-            }
-          }
-
-          item.addEventListener("mouseenter", onMouseEnter)
-          item.addEventListener("mouseleave", onMouseLeave)
-        })
-      }
-      setupHoveredImageBg()
-    }, [wrapperRef, listItemsRefs])
-    return () => ctx.revert() // cleanup
-  }, [])
+  const isDesktopScreen = useMediaQuery("md")
 
   return (
-    <section className="projects-list" ref={wrapperRef}>
+    <section className="projects-list bg-noise" ref={wrapperRef}>
       <Container>
         <h2 className={`projects-list__title`}>Références</h2>
         <div className="projects-list__rows">
           <div className="projects-list__item-hover-bg" ref={itemBgRef}></div>
           {projects.map((project, i) => (
             <ProjectsListRow
-              ref={(el) => (listItemsRefs.current[i] = el)}
               className="projects-list__row"
               key={i}
               {...project}
--- components/Projects/ProjectsListsRow/ProjectsListRow.jsx
+++ components/Projects/ProjectsListsRow/ProjectsListRow.jsx
@@ -1,13 +1,134 @@
-import React, { useRef, forwardRef, useEffect, useLayoutEffect } from "react"
+import React, { useRef, useLayoutEffect } from "react"
 import { gsap } from "common/utils/gsap"
 import cn from "classnames"
+import { useMediaQuery } from "@/common/utils/media-queries"
 
 import "./ProjectsListRow.scss"
 
-export default forwardRef(function ProjectsListRow(
-  { title, team, type, tech, year, className },
-  ref
-) {
+export default function ProjectsListRow({
+  title,
+  team,
+  type,
+  tech,
+  year,
+  className,
+}) {
+  const ref = useRef(null)
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const setupHoverEffect = () => {
+        const c = gsap.utils.selector(ref.current)
+        const bg = c(".projects-list-row__bg")
+        const cells = c(".projects-list-row__cell")
+        const techItems = c(".projects-list-row__tech > span")
+
+        let animation = null
+        let isHovering = false
+
+        const clrBorderLight = "#393435"
+        const clrDark = "#070707"
+        const clrLight = "#ffe8ec"
+
+        const onMouseEnter = (e) => {
+          isHovering = true
+
+          if (!animation) {
+            animation = gsap
+              .timeline({
+                onComplete: () => {
+                  animation = null
+                  if (!isHovering) {
+                    onMouseLeave()
+                  }
+                },
+              })
+              .add("background")
+              .to(bg, {
+                height: "100%",
+                duration: 0.3,
+                ease: "power2.out",
+              })
+              .to(ref.current, {
+                borderColor: clrLight,
+              })
+              .to(
+                cells,
+                {
+                  color: clrDark,
+                  duration: 0.3,
+                },
+                "background"
+              )
+              .to(
+                techItems,
+                {
+                  borderColor: clrDark,
+                  color: clrDark,
+                },
+                "background"
+              )
+          }
+        }
+
+        const onMouseLeave = (e) => {
+          isHovering = false
+
+          if (!animation) {
+            animation = gsap
+              .timeline({
+                onComplete: () => {
+                  animation = null
+                  if (isHovering) {
+                    onMouseEnter()
+                  }
+                },
+              })
+              .add("background")
+              .to(bg, {
+                height: "0",
+                duration: 0.3,
+                ease: "power2.out",
+              })
+              .to(ref.current, {
+                borderColor: clrBorderLight,
+              })
+              .to(
+                cells,
+                {
+                  color: clrLight,
+                  duration: 0.3,
+                },
+                "background"
+              )
+              .to(
+                techItems,
+                {
+                  color: clrLight,
+                  borderColor: clrLight,
+                },
+                "background"
+              )
+          }
+        }
+
+        const addListeners = () => {
+          ref.current.addEventListener("mouseleave", onMouseLeave)
+          ref.current.addEventListener("mouseenter", onMouseEnter)
+        }
+
+        const removeListeners = () => {
+          ref.current.removeEventListener("mouseleave", onMouseLeave)
+          ref.current.removeEventListener("mouseenter", onMouseEnter)
+        }
+
+        addListeners()
+      }
+      setupHoverEffect()
+    }, ref)
+    return () => ctx.revert() // cleanup
+  }, [])
+
   return (
     <a href="/" ref={ref} className={cn(className, "projects-list-row")}>
       <div className="projects-list-row__bg"></div>
@@ -34,4 +155,4 @@ export default forwardRef(function ProjectsListRow(
       <div className="projects-list-row__border"></div>
     </a>
   )
-})
+}
