ed97c76c21a7d1dcbafbc21f7cb1edf38735a137
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-12 13:13:58 +0100

    chore: prettify media-queries.js

--- common/utils/media-queries.js
+++ common/utils/media-queries.js
@@ -1,52 +1,54 @@
+import { useState, useEffect } from "react"
+
 export const sizes = {
-  xs: '576px',
-  sm: '768px',
-  md: '1024px',
-  lg: '1366px',
-  xl: '1920px',
-  xxl: '2560px'
-};
+  xs: "576px",
+  sm: "768px",
+  md: "1024px",
+  lg: "1366px",
+  xl: "1920px",
+  xxl: "2560px",
+}
 
 function isMatch(media) {
-  const query = `(min-width: ${sizes[media]})`;
-  return window.matchMedia(query).matches;
+  const query = `(min-width: ${sizes[media]})`
+  return window.matchMedia(query).matches
 }
 
 function findClosest(queries) {
   for (let i = queries.length - 1; i >= 0; i--) {
     if (isMatch(queries[i])) {
-      return queries[i];
+      return queries[i]
     }
   }
-  return 'sm';
+  return "sm"
 }
 
 export const useClosestMedia = () => {
-  const [closest, setClosest] = useState('sm');
+  const [closest, setClosest] = useState("sm")
 
   useEffect(() => {
-    const listener = () => setClosest(findClosest(queries));
-    listener();
-    window.addEventListener('resize', listener);
-    return () => window.removeEventListener('resize', listener); //Cleanup
-  }, []);
+    const listener = () => setClosest(findClosest(queries))
+    listener()
+    window.addEventListener("resize", listener)
+    return () => window.removeEventListener("resize", listener) //Cleanup
+  }, [])
 
-  return closest;
-};
+  return closest
+}
 
 export const useMediaQuery = (screen) => {
-  const [matches, setMatches] = useState(false);
+  const [matches, setMatches] = useState(false)
 
   useEffect(() => {
-    const query = `(min-width: ${sizes[screen]})`;
-    const media = window.matchMedia(query);
+    const query = `(min-width: ${sizes[screen]})`
+    const media = window.matchMedia(query)
     if (media.matches !== matches) {
-      setMatches(media.matches);
+      setMatches(media.matches)
     }
-    const listener = () => setMatches(media.matches);
-    window.addEventListener('resize', listener);
-    return () => window.removeEventListener('resize', listener);
-  }, [matches, screen]);
+    const listener = () => setMatches(media.matches)
+    window.addEventListener("resize", listener)
+    return () => window.removeEventListener("resize", listener)
+  }, [matches, screen])
 
-  return matches;
-};
+  return matches
+}
