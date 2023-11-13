7f42a62b74121aad6cc39ca8075549994805a4b2
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-13 16:34:26 +0100

    feat: make Github and Linkedin icons

---
+++ components/Icons/IconGithub.jsx
@@ -0,0 +1,17 @@
+const IconGithub = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={17} fill="none">
+    <path
+      stroke="#FFDCE2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M10.905 16.002v-3.048a3.657 3.657 0 0 0-.762-2.667c2.286 0 4.572-1.523 4.572-4.19a4.117 4.117 0 0 0-.762-2.667 5.636 5.636 0 0 0 0-2.666s-.762 0-2.286 1.143a16.396 16.396 0 0 0-6.095 0C4.048.764 3.286.764 3.286.764a5.27 5.27 0 0 0 0 2.666 4.116 4.116 0 0 0-.762 2.667c0 2.667 2.286 4.19 4.572 4.19a3.592 3.592 0 0 0-.762 2.667V16"
+    />
+    <path
+      stroke="#FFDCE2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6.333 12.954C2.897 14.478 2.523 11.431 1 11.431"
+    />
+  </svg>
+)
+export default IconGithub
---
+++ components/Icons/IconLinkedin.jsx
@@ -0,0 +1,11 @@
+const IconLinkedin = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={17} height={19} fill="none">
+    <path
+      stroke="#FFDCE2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.665 5.572a4.571 4.571 0 0 1 4.571 4.572v5.333H13.19v-5.333a1.524 1.524 0 0 0-3.048 0v5.333H7.094v-5.333a4.571 4.571 0 0 1 4.571-4.572ZM4.048 6.335H1v9.143h3.048V6.335ZM2.524 4.05a1.524 1.524 0 1 0 0-3.048 1.524 1.524 0 0 0 0 3.048Z"
+    />
+  </svg>
+)
+export default IconLinkedin
