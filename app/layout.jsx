f1d1becafd7114c92057dacc70f637adcde55505
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-13 14:23:21 +0100

    feat: apply bg-noise to body instead of sections

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={`${trispace.className}`}>
-      <body>
+      <body className="bg-noise">
         {children}
         <LenisScroller />
       </body>
