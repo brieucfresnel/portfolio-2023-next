9f231d65f0dd0999ad77b166f14b6685776daaf1
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-13 14:23:29 +0100

    feat: add Contact to page

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar/Navbar"
 import Header from "@/components/Header/Header"
 import Footer from "@/components/Footer/Footer"
 import Projects from "@/components/Projects/Projects"
+import Contact from "@/components/Contact/Contact"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar></Navbar>
       <Header></Header>
       <Projects></Projects>
+      <Contact></Contact>
       <Footer></Footer>
     </>
   )
