72852d47e928e949050f06c97684024291883d42
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-14 15:36:57 +0100

    feat: add analytics (gtag)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
-import { ReactLenis, useLenis } from "@/components/LenisScroller"
+import { ReactLenis } from "@/components/LenisScroller"
+import Script from "next/script"
 
 import { trispace } from "@/common/utils/fonts"
 import "./global.scss"
@@ -12,6 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={`${trispace.className}`}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-EVDQVZGMWB"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EVDQVZGMWB');
+`}
+      </Script>
       <body className="bg-noise">
         <ReactLenis root>{children}</ReactLenis>
       </body>
