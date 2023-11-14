93a30ca15667af798fafba6baae5665c68d498ca
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-14 14:58:47 +0100

    feat: scroll on contact link click + styles

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
+"use client"
 import React from "react"
 import "./Navbar.scss"
 import Container from "@/components/Container/Container"
+import { useLenis } from "../LenisScroller"
 
 export default function Navbar() {
+  const lenis = useLenis()
+
   return (
     <div className="navbar">
       <Container>
@@ -10,7 +14,14 @@ export default function Navbar() {
           Brieuc
         </a>
         <div className="navbar__menu">
-          <a className="navbar__link" href="mailto:fresnel.brieuc@gmail.com">
+          <a
+            className="navbar__link"
+            href="#contact"
+            onClick={(e) => {
+              e.preventDefault()
+              lenis.scrollTo("#contact")
+            }}
+          >
             Contact
           </a>
         </div>
